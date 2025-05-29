4a62ef1a470bc52c61209e2c8991dce88ae4614f
shivam2898-data <shivamagarwal1432@gmail.com>
2025-05-29 09:08:25 +0530

    introduction and select statement

---
+++ 1st_sql_file.sql
@@ -0,0 +1,15 @@
+select database();
+
+use sakila;
+
+select database();
+
+show databases;
+
+show tables;
+
+select * from actor;
+
+select actor_id, first_name,last_name,first_name from actor;
+
+describe actor;
